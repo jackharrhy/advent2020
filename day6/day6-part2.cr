cd730c7944e9016d8918924feea30f60c023988b
Jack Arthur Harrhy <me@jackharrhy.com>
2020-12-06 10:14:54 -0330

    day 6, part 2, but smoller

--- day6/day6-part2.cr
+++ day6/day6-part2.cr
@@ -1,8 +1,6 @@
-groups = STDIN.gets_to_end.split("\n\n")
-
 count = 0
 
-groups.each do |group|
+STDIN.gets_to_end.split("\n\n").each do |group|
   seen_passwords = ('a'..'z').to_a
   group.split("\n").each do |person|
     next if person == ""
